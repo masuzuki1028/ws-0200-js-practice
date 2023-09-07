dc7afb805b371509d559ce1c3d7fb55cb3fe0951
ma-suzuki <ma-suzuki>
2023-09-07 12:05:02 +0900

    fix

--- 003_practice/002_medium.js
+++ 003_practice/002_medium.js
@@ -93,11 +93,17 @@ function isPalindrome(str) {
 function isPrime(num) {
   if (num === 1) {
     return false
-  } else if (num === 2) {
+  }
+    
+  if (num === 2) {
     return true
   }
 
-  for (let i = 2; i*i <= num; i++) {
+  if (num % 2 === 0) {
+      return false
+  }
+
+  for (let i = 3; i*i <= num; i = i + 2) {
     if ( num % i === 0) {
       return false
     }
